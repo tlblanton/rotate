2b9389bf948e1a833505dc6134f1b6577257db92
tlblanton <tylerblanton635@gmail.com>
2015-12-22 21:57:15 -0700

    imported into intellij

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 import java.util.*;
+import static java.lang.System.out;
 
 public class Main
 {
   public static void main(String[] args)
   {
-    System.out.println("Test");
+    out.println("Imported into intellij");
   }
 }
